db02a92130487ba199be6a7aef170dd296a0ccbf
bjshanahan <bjshanahan@utexas.edu>
2022-12-02 11:05:55 -0600

    Lab 3

--- EDP-380C-28-R2-Lab3.R
+++ EDP-380C-28-R2-Lab3.R
@@ -102,7 +102,7 @@ output
 summary(lm(mpg ~ wt + cyl + gear, data = mtcars))
 
 
-## Generating data for a multiple regression
+## Generating data for a multiple regression by setting the total variance explained
 
 # make function to translate parameter list to inputs expected by rmvnorm(n, mu, Sigma)
 # input, p_x, is a list with the following elements:
@@ -112,55 +112,59 @@ summary(lm(mpg ~ wt + cyl + gear, data = mtcars))
 # sd_x = column vector of standard deviations
 param_translate <- function(p_x) {
   n = n
-  mu = t(mu_x)
-  diag_sigma = diag(c(sd_x[1], sd_x[2]), 2, 2)
-  R_x = matrix(c(1, p_x$cor, p_x$cor, 1), 2, 2)
+  mu = mu_x
+  diag_sigma = diag(c(sd_x))
+  R_x = matrix(p_x$cor, dim(diag_sigma)[1], dim(diag_sigma)[1])
+  diag(R_x) <- 1
   Sigma = diag_sigma %*% R_x %*% diag_sigma
   return(list(n = n, mu = mu, Sigma = Sigma))
 }
 
-# generate X1 and X2 using rmvnorm() function with the following parameters;
+# generate X1 and X2 using rmvnorm() function with the following parameters:
 
 set.seed(21389)
 n <- 100000
 
-# set correlation to 0.3
+# set correlation between predictors to 0.3
 r12 <- 0.3
 
 # set mean vector
-mu_x <- matrix(c(5, 10), nrow = 1, ncol = 2)
+x <- c(5, 10)
+mu_x <- matrix(x, nrow = length(x), ncol = 1)
 
 # set st dev
-sd_x <- matrix(1:2, nrow = 2, ncol = 1)
+sd_x <- 1:2
+sd_x <- matrix(sd_x, nrow = length(sd_x), ncol = 1)
 
 p_x <- list(n = n, cor = r12, mu_x = mu_x, sd_x = sd_x)
 
+# use function to translate parameter list to inputs expected by rmvnorm(n, mu, Sigma)
 param_rmvnorm <- param_translate(p_x)
 
+# generate X given parameters
 X <- rmvnorm(param_rmvnorm$n, param_rmvnorm$mu, param_rmvnorm$Sigma)
 
-
+# set y parameters
 set.seed(23921)
 
-x1 = X[, 1]
-x2 = X[, 2]
 b1 = 1
 b2 = 1
-beta = matrix(c(b1, b2), nrow = 2, ncol = 1)
+betas = matrix(c(b1, b2), nrow = 2, ncol = 1)
 R2 = 0.6
 mu_y = 10
 sd_y = 5
-p_y <- list(X = X, b1 = b1, b2 = b2, R2 = R2, mu_y = mu_y, sd_y = sd_y)
+p_y <- list(betas = betas, R2 = R2, mu_y = mu_y, sd_y = sd_y)
 
-generate_y_setR2 <- function(p_y) {
-  B = matrix(c(p_y$b1, p_y$b2), ncol = 1)
-  b0 = mu_y - mu_x %*% B
-  Sigma_x <- cov(p_y$X)
+# generate y setting the total variance explained
+generate_y_R2 <- function(X, p_y) {
+  B = matrix(betas, ncol = 1)
+  b0 = mu_y - t(mu_x) %*% B
+  Sigma_x <- cov(X)
   sd_e <- sqrt( (t(B) %*% Sigma_x %*% B) %*% ((1 / R2) - 1) )
   y = rnorm(n, as.vector(b0) + X %*% B, sd_e)
 }
 
-y <- generate_y_setR2(p_y)
+y <- generate_y_R2(X, p_y)
 
 solution3b <- produce_OLS_solution(y, X)
 
@@ -184,6 +188,8 @@ diff_y_param <- list(mean = mean(y) - mu_y, sd = sd(y) - sd_y)
 #[1] -1.78886
 
 
+## Generating data setting the marginal correlations (rhoyx1, rhoyx2...)
+
 set.seed(123782)
 x1 = X[, 1]
 x2 = X[, 2]
@@ -192,7 +198,7 @@ rho_yx2 = -0.4
 mu_y = 10
 sd_y = 5
 param_y_3c <- list(X = X,
-                   mu_x = matrix(c(mean(X[,1]), mean(X[,2])), nrow = 1),
+                   mu_x = matrix(c(mean(X[,1]), mean(X[,2])), ncol = 1),
                    rho_yx1 =  0.3,
                    rho_yx2 = -0.4,
                    mu_y = 10,
@@ -207,7 +213,7 @@ generate_y_setRhoxy <- function(p_y) {
   R_x = as.matrix(cor(X))
   R_xy = t(R_yx)
   R2 = R_xy %*% solve(R_x) %*% R_yx
-  b0 = mu_y - mu_x %*% beta
+  b0 = mu_y - t(mu_x) %*% beta
   Sigma_x <- cov(p_y$X)
   sd_e <- sqrt( (t(beta) %*% Sigma_x %*% beta) %*% ((1 / R2) - 1) )
   y = rnorm(n, as.vector(b0) + X %*% beta, sd_e)
@@ -237,20 +243,163 @@ diff_y_param_3c
 
 
 
-gen_data_setR2 <- function(n, p_x, p_y) {
+# Data generation setting total variance explained
 
+set.seed(6972)
+# set parameters
+n <- 100000
+rho <- 0.15
+
+# set mean vector
+x <- c(2:6)
+mu_x <- matrix(x, nrow = length(x), ncol = 1)
+
+# set st dev
+sd_x <- sqrt(1:5)
+sd_x <- matrix(sd_x, nrow = length(sd_x), ncol = 1)
+
+p_x <- list(n = n, cor = rho, mu_x = mu_x, sd_x = sd_x)
+
+# create Covariance matrix, Sigma, from correlations and variances of x predictors
+p_x <- param_translate(p_x)
+
+# set p_y
+b1 = 1
+b2 = 1
+b3 = 1
+b4 = 1
+b5 = 1
+betas = matrix(c(b1, b2, b3, b4, b5), nrow = 5, ncol = 1)
+R2 = 0.5
+mu_y = 25
+sd_y = 5
+b0 = mu_y - t(mu_x) %*% betas
+p_y <- list(intercept = b0, betas = betas, R2 = R2, mu_y = mu_y, sd_y = sd_y)
+
+gen_data_setR2 <- function(n, p_x, p_y) {
+  # generate X
+  X = rmvnorm(p_x$n, p_x$mu, p_x$Sigma)
+  y = generate_y_R2(X, p_y)
+  output_data <- list(X = X, y = y)
+  # add as attributes to the output the population parameters
+  attr(output_data, 'pop param') <- p_y
+  return(output_data)
 }
 
+data_method_1 <- gen_data_setR2(n, p_x, p_y)
+data_method_1
 
-gen_data_setRhoxy <-
+solution3.d.2 <- produce_OLS_solution(data_method_1$y, data_method_1$X)
+solution3.d.2
 
-# shortcuts
-tcrossprod(X) = X %*% t(X)
-crossprod(X) = t(X) %*% X
-crossprod(X, Y) = t(X) %*% Y
+# > solution3.d.2
+# Estimate                  SE          t_value p_value
+# b0    5.0905785  0.0650750923269285 78.2262203281901       0
+# b1    0.9812633  0.0156515010851895  62.694517412879       0
+# b2    1.0199224  0.0110811906847618 92.0408646338139       0
+# b3    0.9881525 0.00905975770179736 109.070524541459       0
+# b4    0.9852174 0.00784277723624359 125.620985738752       0
+# b5    1.0050576 0.00705536110150286 142.453033477147       0
+# SD(e) 4.8135804                  NA               NA      NA
+# R2    0.4991120                  NA               NA      NA
 
-solve(crossprod(X) %*% crossprod(X, Y)) =
-solve((crossprod(X)), crossprod(X, Y))
+pop_param <- matrix(c(
+  intercept = attr(data_method_1,"pop param")$intercept,
+  b1 = attr(data_method_1,"pop param")$betas[1, 1],
+  b2 = attr(data_method_1,"pop param")$betas[2, 1],
+  b3 = attr(data_method_1,"pop param")$betas[3, 1],
+  b4 = attr(data_method_1,"pop param")$betas[4, 1],
+  b5 = attr(data_method_1,"pop param")$betas[5, 1],
+  SD_e = attr(data_method_1,"pop param")$sd_y,
+  R2 = attr(data_method_1,"pop param")$R2 ), 8, 1)
+
+beta_diff <- matrix(c(
+  intercept = solution3.d.2[1, 1] - attr(data_method_1,"pop param")$intercept,
+  b1 = solution3.d.2[2, 1] - attr(data_method_1,"pop param")$betas[1, 1],
+  b2 = solution3.d.2[3, 1] - attr(data_method_1,"pop param")$betas[2, 1],
+  b3 = solution3.d.2[4, 1] - attr(data_method_1,"pop param")$betas[3, 1],
+  b4 = solution3.d.2[5, 1] - attr(data_method_1,"pop param")$betas[4, 1],
+  b5 = solution3.d.2[6, 1] - attr(data_method_1,"pop param")$betas[5, 1],
+  SD_e = solution3.d.2[7, 1] - attr(data_method_1,"pop param")$sd_y,
+  R2 = solution3.d.2[8, 1] - attr(data_method_1,"pop param")$R2 ), 8, 1)
+
+Solution = data.frame(Estimate = solution3.d.2[, 1],
+                    Paramater = pop_param,
+                    Difference = beta_diff )
+
+# > Solution
+#    Estimate Paramater    Difference
+# 1 5.0905785       5.0  0.0905785102
+# 2 0.9812633       1.0 -0.0187366927
+# 3 1.0199224       1.0  0.0199223718
+# 4 0.9881525       1.0 -0.0118474752
+# 5 0.9852174       1.0 -0.0147825927
+# 6 1.0050576       1.0  0.0050575912
+# 7 4.8135804       5.0 -0.1864196485
+# 8 0.4991120       0.5 -0.0008880124
+
+
+# Data generation setting marginal correlations
+
+set.seed(1237)
+# set parameters
+n <- 100000
+rho <- 0.15
+
+# set mean vector
+x <- c(2:6)
+mu_x <- matrix(x, nrow = length(x), ncol = 1)
+
+# set st dev
+sd_x <- sqrt(1:5)
+sd_x <- matrix(sd_x, nrow = length(sd_x), ncol = 1)
+
+p_x <- list(n = n, cor = rho, mu_x = mu_x, sd_x = sd_x)
+
+p_y <- list(rho_yx1 = -0.15,
+            rho_yx2 = -0.50,
+            rho_yx3 = 0.15,
+            rho_yx4 = 0.30,
+            rho_yx5 = .20,
+            mu_y = 10,
+            sd_y = 4)
+
+# generate X covariance matrix
+
+# unused function
+# gen_covX <- function(X) {
+# S_x = cov(X)
+# sd_X = matrix(apply(X, 2, sd))
+# rho_yx = c(rho_yx1 = -0.15,
+#            rho_yx2 = -0.50,
+#            rho_yx3 = 0.15,
+#            rho_yx4 = 0.30,
+#            rho_yx5 = .20)
+# S_xy = matrix(c((rho_yx * sd_X * sd_y)))
+# }
+
+# computing the covariance matrix
+
+
+gen_data_setRhoxy <- function(n, p_x, p_y) {
+  #  # create Covariance matrix, Sigma, from correlations and variances of x predictors
+  param_rmvnorm <- param_translate(p_x)
+  # generate X
+  X <- rmvnorm(param_rmvnorm$n, param_rmvnorm$mu, param_rmvnorm$Sigma)
+
+  # generate Y
+  y = rnorm(n, as.vector(b0) + X %*% beta, sd_e)
+  cbind(X, y)
+}
+
+data_method_2 <- gen_data_setRhoxy(n, p_x, p_y)
+data_method_2
+
+solution3.d.2 <- produce_OLS_solution(y, X)
+solution3.d.2
+
+diff_y_param <- list(mean = mean(y) - mu_y, sd = sd(y) - sd_y)
+diff_y_param
 
 
 
--- crossprod() and tcrossprod().R
+++ crossprod() and tcrossprod().R
@@ -4,4 +4,4 @@ crossprod(X) = t(X) %*% X
 crossprod(X, Y) = t(X) %*% Y
 
 solve(crossprod(X) %*% crossprod(X, Y)) =
-  solve((crossprod(X)), crossprod(X, Y))
+solve((crossprod(X)), crossprod(X, Y))
